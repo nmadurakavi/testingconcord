bd47eecd69ed25c9d5ec6e6a706985b9e291ccfe
Nag Madurakavi <nagender.madurkavi@walmartlabs.com>
2020-09-29 21:25:40 -0700

    test

--- example.py
+++ example.py
@@ -1,1 +1,7 @@
 import sys
+
+sys.path.append(".")
+
+import my_module
+
+my_module.test(execution,x,a,b,c,d,e)
--- my_module.py
+++ my_module.py
@@ -1,1 +1,4 @@
-def 
+def test(ctx, x, a, b, c, d, e):
+	print "Hello, I am in module"
+	print x, a, b, c, d, e
+	ctx.setVariable('y', e + 3)
